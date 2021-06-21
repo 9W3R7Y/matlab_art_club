e28e18ae1e31b3041a3cdee5a5ed632b76ac323d
qwerty_16180339 <qwerty.16180339@gmail.com>
2021-06-22 01:33:12 +0900

    curl noiise

--- 06_Curl_Nosie_for_Fluid/Curl_Noise_script.m
+++ 06_Curl_Nosie_for_Fluid/Curl_Noise_script.m
@@ -1,38 +1,69 @@
-seed        =  25;
-size        = 512;
-n_tiles     =   5;
-n_octaves   =   10;
-persistence =0.315;
+%{
+seed        =   32;
+size        =  512;
+n_tiles     =     5;
+n_octaves   =   28;
+persistence = 0.706;
+noise       =   64.7;
+pointForce  =  10.8;
+vel         = 9.2;
+n_particles = 5950000;
+END_TIME =     100;
+%}
+
+seed        =   32;
+size        =  512;
+n_tiles     =     5;
+n_octaves   =   28;
+persistence = 0.706;
+noise       =   64.7;
+pointForce  =  50;
+vel         = 9.2;
+n_particles = 5950000;
+END_TIME =     100;
 
 rng(seed);
 
-Psi      = zeros(size);
+PsiN       = zeros(size);
 idxs    = 1:n_octaves;
 max_amp = sum(persistence.^(idxs-1));
 
 for i = idxs
-    Psi = Psi + ...
+    PsiN = PsiN + ...
         imresize(rand(n_tiles*i), ...
                 [size size],"cubic")*...
         persistence^(i-1)/...
         max_amp;
 end
 
-Psi = Psi./n_tiles;
+PsiN = PsiN/n_tiles;
 
-[dNdx,dNdy] = gradient(Psi);
+x = (1:size)/size-1/2;
+y = (1:size)/size-1/2;
 
-vx =  dNdy;
-vy = -dNdx;
+PsiP = sqrt(x.^2+transpose(y.^2));
 
-n_particles = 100;
-END_TIME = 100;
+Psi = vel*(PsiN*noise+PsiP*pointForce);
 
-dt = 1;
-A = 100 * size;
+[dPsidx,dPsidy] = gradient(Psi);
 
-px = (size-2)*rand(n_particles,1)+1;
-py = (size-2)*rand(n_particles,1)+1;
+vx =   dPsidx;
+vy =  -dPsidy;
+
+vx = vx*size;
+vy = vy*size;
+
+dt = 1/60;
+
+%init
+[px,pidxs] = sort(size*rand(n_particles,1));
+py = size*rand(n_particles,1);
+pc = 1:n_particles;
+pc = pc(pidxs);
+
+fig = figure('Position',[0,0,800,800]);
+
+frames(END_TIME) = struct('cdata', [], 'colormap', []);
 
 for t = 1:END_TIME
     px_grid = round(px);
@@ -44,21 +75,22 @@ for t = 1:END_TIME
     py_grid(py_grid<1) = 1;
     py_grid(size<py_grid) = size;
     
+    cla
+    scatter(px,py,1,pc,".","CData",hsv(n_particles));
+    xlim([0 size]);
+    ylim([0 size]);
+    pbaspect([1 1 1]);
+    axis off
+    
+    saveas(fig,"results\"+num2str(t,'%04.f')+".png");
+    
+    frames(i) = getframe(fig);
+    
     for i = 1:n_particles
         pvx = vx(px_grid(i),py_grid(i));
         pvy = vy(px_grid(i),py_grid(i));
         
-        px(i) = px(i) + A * pvx * dt;
-        py(i) = py(i) + A * pvy * dt;
-    end
-    
-    cla;
-    scatter(px,py);
-    hold on
-    quiver(vx,vy);
-    xlim([0,size]);
-    ylim([0,size]);
-    pbaspect([1 1 1]);
-    drawnow;
-    
+        px(i) = px(i) + pvx * dt;
+        py(i) = py(i) + pvy * dt;
+    end 
 end
